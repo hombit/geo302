9b1a82461a11a9e6cb281d005b0069e488e41629
Konstantin Malanchev <hombit@gmail.com>
2022-11-21 15:37:57 -0600

    Test for config loading

--- src/config.rs
+++ src/config.rs
@@ -108,3 +108,102 @@ pub fn parse_config<P: AsRef<Path>>(path: P) -> anyhow::Result<Config> {
     let config: Config = toml::from_str(&toml_string)?;
     Ok(config)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use include_dir::{include_dir, Dir};
+
+    const CONFIG_EXAMPLES: Dir = include_dir!("$CARGO_MANIFEST_DIR/config-examples");
+
+    #[cfg(all(
+        feature = "maxminddb",
+        feature = "ripe-geo-embedded",
+        feature = "ripe-geo-autoupdate"
+    ))]
+    #[test]
+    fn load_all_config_examples() {
+        for entry in CONFIG_EXAMPLES.find("**/*.toml").unwrap() {
+            let file = entry.as_file().unwrap();
+            let toml_string = file.contents_utf8().unwrap();
+            let result: Result<Config, _> = toml::from_str(toml_string);
+            assert!(result.is_ok());
+        }
+    }
+
+    fn load_from_example_config(s: &'static str) -> Result<Config, toml::de::Error> {
+        let file = CONFIG_EXAMPLES.get_file(s).unwrap();
+        let toml_string = file.contents_utf8().unwrap();
+        toml::from_str(toml_string)
+    }
+
+    macro_rules! load_config {
+        ($name: ident, $file: expr, $($feature: expr $(,)?)+) => {
+            #[test]
+            fn $name() {
+                #[cfg(all(
+                    $(
+                        feature = $feature,
+                    )*
+                ))]
+                assert!(load_from_example_config($file).is_ok(), "must be Ok");
+                #[cfg(not(any(
+                    $(
+                        feature = $feature,
+                    )*
+                )))]
+                assert!(load_from_example_config($file).is_err(), "must be Err");
+            }
+        };
+    }
+
+    load_config!(load_maxminddb_config, "maxmind-db.toml", "maxminddb");
+
+    load_config!(
+        load_ripe_geo_autoupdate_no_dir_1,
+        "ripe-geo-autoupdate-no-dir-1.toml",
+        "ripe-geo-autoupdate"
+    );
+    load_config!(
+        load_ripe_geo_autoupdate_no_dir_2,
+        "ripe-geo-autoupdate-no-dir-2.toml",
+        "ripe-geo-autoupdate"
+    );
+    load_config!(
+        load_ripe_geo_autoupdate_no_dir_3,
+        "ripe-geo-autoupdate-no-dir-3.toml",
+        "ripe-geo-autoupdate"
+    );
+    load_config!(
+        load_ripe_geo_autoupdate_no_dir_4,
+        "ripe-geo-autoupdate-no-dir-4.toml",
+        "ripe-geo-autoupdate"
+    );
+
+    // Negative test doesn't work well here
+    #[cfg(feature = "ripe-geo-embedded")]
+    #[test]
+    fn load_ripe_geo_embedded_no_autoupdate_1() {
+        let result: Result<Config, _> =
+            load_from_example_config("ripe-geo-embedded-no-autoupdate-1.toml");
+        assert!(result.is_ok());
+    }
+    load_config!(
+        load_ripe_geo_embedded_no_autoupdate_2,
+        "ripe-geo-embedded-no-autoupdate-2.toml",
+        "ripe-geo-autoupdate",
+        "ripe-geo-embedded"
+    );
+
+    load_config!(
+        load_ripe_geo_from_dir_no_autoupdate_1,
+        "ripe-geo-from-dir-no-autoupdate-1.toml",
+        "ripe-geo"
+    );
+    load_config!(
+        load_ripe_geo_from_dir_no_autoupdate_2,
+        "ripe-geo-from-dir-no-autoupdate-2.toml",
+        "ripe-geo-autoupdate"
+    );
+}
